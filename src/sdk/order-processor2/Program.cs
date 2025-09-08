95aa52873675734595f90c98fd4b48008d287c53
Jan Kasper <jan.kasper@t2informatik.de>
2025-09-08 12:41:08 +0200

    Have two worker consume from same queue

--- src/sdk/order-processor2/Program.cs
+++ src/sdk/order-processor2/Program.cs
@@ -1,7 +1,7 @@
 using System.Text.Json.Serialization;
 using Dapr;
 
-//run with : PS C:\Git\DaprPlayground\src\sdk\order-processor> dapr run --app-id order-processor --resources-path ../../../components --app-port 7006 -- dotnet run
+//run with: PS C:\Git\DaprPlayground\src\sdk\order-processor2> dapr run --app-id order-processor --resources-path ../../../components --app-port 7007 -- dotnet run
 //NOTE: --app-id has to be identical with other worker tasks, so they use the same queue, otherwise the get separate queues! 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,8 +18,8 @@ if (app.Environment.IsDevelopment()) {app.UseDeveloperExceptionPage();}
 
 // Dapr subscription in [Topic] routes orders topic to this route
 app.MapPost("/orders", [Topic("redispubsub", "orders")] async (Order order) => {
-    Console.WriteLine("Subscriber 1 received : " + order);
-    await Task.Delay(TimeSpan.FromSeconds(4.2));
+    Console.WriteLine("Subscriber 2 received : " + order);
+    await Task.Delay(TimeSpan.FromSeconds(1.69));
     return Results.Ok(order);
 });
 
