5806b0335c24a444defcc90d16f3c7e66538ee29
Jan Kasper <jan.kasper@t2informatik.de>
2025-09-09 09:15:11 +0200

    add a database, both worker can access it

--- src/sdk/order-processor/Program.cs
+++ src/sdk/order-processor/Program.cs
@@ -1,10 +1,12 @@
 using System.Text.Json.Serialization;
 using Dapr;
+using Dapr.Client;
 
 //run with : PS C:\Git\DaprPlayground\src\sdk\order-processor> dapr run --app-id order-processor --resources-path ../../../components --app-port 7006 -- dotnet run
 //NOTE: --app-id has to be identical with other worker tasks, so they use the same queue, otherwise the get separate queues! 
-
+const string DAPR_STORE_NAME = "statestore";
 var builder = WebApplication.CreateBuilder(args);
+var client = new DaprClientBuilder().Build();
 
 var app = builder.Build();
 
@@ -20,8 +22,14 @@ if (app.Environment.IsDevelopment()) {app.UseDeveloperExceptionPage();}
 app.MapPost("/orders", [Topic("redispubsub", "orders")] async (Order order) => {
     Console.WriteLine("Subscriber 1 received : " + order);
     await Task.Delay(TimeSpan.FromSeconds(4.2));
+    var amount = await client.GetStateAsync<int>(DAPR_STORE_NAME, "Amount") +1;
+    Console.WriteLine("Overall received packages in Subscriber 1: " + amount);
+    await client.SaveStateAsync(DAPR_STORE_NAME, "Amount", amount);
+
     return Results.Ok(order);
 });
+
+
 
 await app.RunAsync();
 
--- src/sdk/order-processor2/Program.cs
+++ src/sdk/order-processor2/Program.cs
@@ -1,11 +1,13 @@
 using System.Text.Json.Serialization;
 using Dapr;
+using Dapr.Client;
 
 //run with: PS C:\Git\DaprPlayground\src\sdk\order-processor2> dapr run --app-id order-processor --resources-path ../../../components --app-port 7007 -- dotnet run
 //NOTE: --app-id has to be identical with other worker tasks, so they use the same queue, otherwise the get separate queues! 
 
+const string DAPR_STORE_NAME = "statestore";
 var builder = WebApplication.CreateBuilder(args);
-
+var client = new DaprClientBuilder().Build();
 var app = builder.Build();
 
 // Dapr will send serialized event object vs. being raw CloudEvent
@@ -19,6 +21,13 @@ if (app.Environment.IsDevelopment()) {app.UseDeveloperExceptionPage();}
 // Dapr subscription in [Topic] routes orders topic to this route
 app.MapPost("/orders", [Topic("redispubsub", "orders")] async (Order order) => {
     Console.WriteLine("Subscriber 2 received : " + order);
+    
+    var amount = await client.GetStateAsync<int>(DAPR_STORE_NAME, "Amount");
+    var amount2 = await client.GetStateAsync<int>(DAPR_STORE_NAME, "Amount2") +1;
+    Console.WriteLine("Overall received packages in Subscriber 1: " + amount);
+    Console.WriteLine("Overall received packages in Subscriber 2: " + amount2);
+    await client.SaveStateAsync(DAPR_STORE_NAME, "Amount2", amount2);
+    
     await Task.Delay(TimeSpan.FromSeconds(1.69));
     return Results.Ok(order);
 });
